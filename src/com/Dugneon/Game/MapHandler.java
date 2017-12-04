96503d32cb39ef7c988049841ea42fae3f9161fb
gatewaytofredom <bobdosgames@gmail.com>
2017-12-04 11:40:34 -0600

    Half assed Map/room update
    Started to work on map generation kinda

--- src/com/Dugneon/Game/MapHandler.java
+++ src/com/Dugneon/Game/MapHandler.java
@@ -1,5 +1,35 @@
 package com.Dugneon.Game;
 
-public class MapHandler {
+import java.util.ArrayList;
 
+public class MapHandler {
+	
+	int MapX = 10;
+	int MapY = 10;
+	
+	
+	
+	Room[][] Map = new Room[MapY][MapX];
+	
+	//Now to generate the map
+	
+	//loop through the MapY
+	for (int i=0; i<MapY; i++) {
+		
+		//loop through the MapX
+		for (int j=0; j<MapX; j++) {
+			if (i=i)
+			
+			
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
 }
+}
+
