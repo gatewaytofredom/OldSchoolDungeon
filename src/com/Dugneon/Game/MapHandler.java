9d91c6689af77cbab845e85226a5864da510d665
gatewaytofredom <bobdosgames@gmail.com>
2017-12-04 21:42:35 -0600

    insignifcant update for maphandler
    Generates Top row of walls now (maybe)

--- src/com/Dugneon/Game/MapHandler.java
+++ src/com/Dugneon/Game/MapHandler.java
@@ -5,39 +5,42 @@ import java.util.ArrayList;
 import com.Dugneon.Game.Room.wallType;
 
 public class MapHandler {
-	
+
 	int MapX = 10;
 	int MapY = 10;
-	
-	
-	
-	Room[][] Map = new Room[MapY][MapX];{
-	
-	//Now to generate the map
-	
-	//loop through the MapY
-	for (int i=0; i<MapY; i++) {
-		
-		//loop through the MapX
-		for (int j=0; j<MapX; j++) {
-			//if area is north or south map edge, generate a room with all walls
-			if (i==0 || i==MapY) {
-				Map[i][j] = new Room();
-				Map[i][j].setWallTypes(wallType.Brick, wallType.Brick, wallType.Brick, wallType.Brick);
-				
-				
-			}
-			
-			
-		}
-		
-		
-	}
-	
 
-	
-	
-	
-}
+
+
+	Room[][] Map = new Room[MapY][MapX];{    //WHY DOES THIS WORK IM SO CONFUSED ;{
+
+		//Now to generate the map
+
+		//loop through the MapY
+		for (int i=0; i<MapY; i++) {
+
+			//loop through the MapX
+			for (int j=0; j<MapX; j++) {
+				//if area is north or south map edge, generate a room with all walls
+				if (i==0 || i==MapY) {
+					Map[i][j] = new Room();
+
+					//if top or bottom edge of map generate all walls
+					if((i==0 || i==MapY ) && (j==0 || j==MapX)) {
+						Map[i][j].setWallTypes(wallType.Brick, wallType.Brick, wallType.Brick, wallType.Brick);
+					}
+
+				}
+
+
+			}
+
+
+		}
+
+
+
+
+
+	}
 }
 
